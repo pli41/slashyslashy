75e2f0b260695cfd4e825af57983be8382073820
Peilin Li <lipeilin@gatech.edu>
2016-02-05 16:30:52 -0500

    Initial level setup

---
+++ Assets/CameraFollow2D.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow2D : MonoBehaviour {
+
+    public GameObject target;
+
+    public float MaxFollowingSpeed;
+    
+
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (CheckDistanceToTarget())
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, target.transform.position, MaxFollowingSpeed);
+            transform.position = new Vector3(transform.position.x, 1, -10);
+        }
+	}
+
+
+    /// <summary>
+    /// Checks distance from camera to target
+    /// </summary>
+    /// <returns>returns true when too far; vise versa</returns>
+    bool CheckDistanceToTarget()
+    {
+        float distance = Vector2.Distance(transform.position, target.transform.position);
+        if (distance > 0.1f)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public Animator animator;
+    public Rigidbody2D rigid;
+
+    public enum PlayerState{ Run, Attack, Jump, Def};
+    public PlayerState state;
+
+    public float MaxHoriSpeed;
+    public float accelerationTime;
+
+
+    public float currentAccTime;
+    public bool recovering;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+	// Use this for initialization
+	void Start () {
+        currentAccTime = 0f;
+
+	}
+	
+
+
+	// Update is called once per frame
+	void Update () {
+        if (state == PlayerState.Run)
+        {
+            rigid.velocity = RecoverSpeed();
+        }
+        else if(state == PlayerState.Attack)
+        {
+
+        }
+        else if (state == PlayerState.Jump)
+        {
+
+        }
+        else if (state == PlayerState.Def)
+        {
+
+        }
+    }
+
+    Vector2 RecoverSpeed()
+    {
+        if (rigid.velocity.x < MaxHoriSpeed)
+        {
+            HandleRecoveryAccTime();
+        }
+        Vector2 currentVelo = rigid.velocity;
+        Vector2 nextStepVelo = Vector2.Lerp(currentVelo, new Vector2(MaxHoriSpeed, rigid.velocity.y), currentAccTime);
+        return nextStepVelo;
+    }
+
+    void HandleRecoveryAccTime()
+    {
+        if (currentAccTime < 1f)
+        {
+            currentAccTime += Time.deltaTime;
+        }
+    }
+
+
+}
